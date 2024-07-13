bdf684df8a66e892c209ab518982b8f5bea67408
Mason Ott <mason.xavier.ott@gmail.com>
2024-07-13 17:38:39 -0600

    updated timer and chef note image

--- assets/js/cooking.js
+++ assets/js/cooking.js
@@ -2,19 +2,22 @@ const timer1 = document.querySelector("#timer1")
 
 let timer1value = 3600
 function timerC1() {
-if (this.textContent==="Timer"){
+if (this.textContent === "1 Hour Timer"){
+let interval = setInterval(function() {
+    if (timer1value > 0) {
+    timer1value-=1;
+    let minute = Math.floor(timer1value / 60);
+    let seconds = timer1value % 60;
 
-setInterval(function(){
-timer1value-=1
-timer1.textContent = timer1value
-},1000)
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    timer1.textContent =  `${minute}:${seconds}`;
+} else{
+    clearInterval(interval);
+}
+ }, 1000)
 }
 }
 timer1.addEventListener("click", timerC1)
-
-
-
-
 
 
 
@@ -23,16 +26,22 @@ const timer2 = document.querySelector("#timer2")
 
 let timer2value = 120
 function timerC2() {
-if (this.textContent==="Timer"){
+if (this.textContent === "Timer"){
+let interval = setInterval(function() {
+    if (timer2value > 0) {
+    timer2value-=1;
+    let minute = Math.floor(timer2value / 60);
+    let seconds = timer2value % 60;
 
-setInterval(function(){
-timer2value-=1
-timer2.textContent = timer2value
-},1000)
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    timer2.textContent =  `${minute}:${seconds}`;
+} else{
+    clearInterval(interval);
+}
+ }, 1000)
 }
 }
 timer2.addEventListener("click", timerC2)
-
 
 
 
@@ -40,12 +49,19 @@ const timer3 = document.querySelector("#timer3")
 
 let timer3value = 600
 function timerC3() {
-if (this.textContent==="Timer"){
+if (this.textContent === "Timer"){
+let interval = setInterval(function() {
+    if (timer3value > 0) {
+    timer3value-=1;
+    let minute = Math.floor(timer3value / 60);
+    let seconds = timer3value % 60;
 
-setInterval(function(){
-timer3value-=1
-timer3.textContent = timer3value
-},1000)
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    timer3.textContent =  `${minute}:${seconds}`;
+} else{
+    clearInterval(interval);
+}
+ }, 1000)
 }
 }
 timer3.addEventListener("click", timerC3)
