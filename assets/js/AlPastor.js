e42b120ec207cc8aaa370d39696106415501f849
Mason Ott <mason.xavier.ott@gmail.com>
2024-07-14 13:43:23 -0600

    updated Al Pastor HTML with timers and chef notes and completed functions for timers

--- assets/js/AlPastor.js
+++ assets/js/AlPastor.js
@@ -18,3 +18,51 @@ let interval = setInterval(function() {
 }
 }
 timer10.addEventListener("click", timerC10);
+
+
+const timer11 = document.querySelector("#timer11")
+
+let timer11value = 86400;
+function timerC11() {
+if (this.textContent === "24 Hour Timer"){
+let interval = setInterval(function() {
+    if (timer11value > 0) {
+    timer11value-=1;
+    let hours = Math.floor(timer11value / 3600);
+    let minutes = Math.floor(( timer11value % 3600) /60);
+    let seconds = timer11value % 60;
+
+    minutes= minutes <10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    timer11.textContent =  `${hours}:${minutes}:${seconds}`;
+} else{
+    clearInterval(interval);
+}
+ }, 1000)
+}
+}
+timer11.addEventListener("click", timerC11);
+
+
+
+const timer12 = document.querySelector("#timer12")
+
+let timer12value = 600;
+function timerC12() {
+if (this.textContent === "10 Min Timer"){
+let interval = setInterval(function() {
+    if (timer12value > 0) {
+    timer12value-=1;
+    let minute = Math.floor(timer12value / 60);
+    let seconds = timer12value % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    timer12.textContent =  `${minute}:${seconds}`;
+} else{
+    clearInterval(interval);
+}
+ }, 1000)
+}
+}
+timer12.addEventListener("click", timerC12);
