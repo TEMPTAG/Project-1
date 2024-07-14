2a4415571d9d127cf9321c9fbdc348b514e9c008
Ian Ferguson <iansterlingferguson@gmail.com>
2024-07-14 15:21:56 -0700

    starting code and file cleanup

--- assets/js/AlPastor.js
+++ assets/js/AlPastor.js
@@ -1,68 +1,65 @@
-const timer10 = document.querySelector("#timer10")
+const timer10 = document.querySelector("#timer10");
 
 let timer10value = 120;
 function timerC10() {
-if (this.textContent === "2 Min Timer"){
-let interval = setInterval(function() {
-    if (timer10value > 0) {
-    timer10value-=1;
-    let minute = Math.floor(timer10value / 60);
-    let seconds = timer10value % 60;
+  if (this.textContent === "2 Min Timer") {
+    let interval = setInterval(function () {
+      if (timer10value > 0) {
+        timer10value -= 1;
+        let minute = Math.floor(timer10value / 60);
+        let seconds = timer10value % 60;
 
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    timer10.textContent =  `${minute}:${seconds}`;
-} else{
-    clearInterval(interval);
-}
- }, 1000)
-}
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        timer10.textContent = `${minute}:${seconds}`;
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
 }
 timer10.addEventListener("click", timerC10);
 
-
-const timer11 = document.querySelector("#timer11")
+const timer11 = document.querySelector("#timer11");
 
 let timer11value = 86400;
 function timerC11() {
-if (this.textContent === "24 Hour Timer"){
-let interval = setInterval(function() {
-    if (timer11value > 0) {
-    timer11value-=1;
-    let hours = Math.floor(timer11value / 3600);
-    let minutes = Math.floor(( timer11value % 3600) /60);
-    let seconds = timer11value % 60;
+  if (this.textContent === "24 Hour Timer") {
+    let interval = setInterval(function () {
+      if (timer11value > 0) {
+        timer11value -= 1;
+        let hours = Math.floor(timer11value / 3600);
+        let minutes = Math.floor((timer11value % 3600) / 60);
+        let seconds = timer11value % 60;
 
-    minutes= minutes <10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    timer11.textContent =  `${hours}:${minutes}:${seconds}`;
-} else{
-    clearInterval(interval);
-}
- }, 1000)
-}
+        timer11.textContent = `${hours}:${minutes}:${seconds}`;
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
 }
 timer11.addEventListener("click", timerC11);
 
-
-
-const timer12 = document.querySelector("#timer12")
+const timer12 = document.querySelector("#timer12");
 
 let timer12value = 600;
 function timerC12() {
-if (this.textContent === "10 Min Timer"){
-let interval = setInterval(function() {
-    if (timer12value > 0) {
-    timer12value-=1;
-    let minute = Math.floor(timer12value / 60);
-    let seconds = timer12value % 60;
+  if (this.textContent === "10 Min Timer") {
+    let interval = setInterval(function () {
+      if (timer12value > 0) {
+        timer12value -= 1;
+        let minute = Math.floor(timer12value / 60);
+        let seconds = timer12value % 60;
 
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    timer12.textContent =  `${minute}:${seconds}`;
-} else{
-    clearInterval(interval);
-}
- }, 1000)
-}
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        timer12.textContent = `${minute}:${seconds}`;
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
 }
 timer12.addEventListener("click", timerC12);
--- assets/js/birriaBeef.js
+++ assets/js/birriaBeef.js
@@ -1,45 +1,44 @@
-const timer8 = document.querySelector("#timer8")
+const timer8 = document.querySelector("#timer8");
 
 let timer8value = 600;
 function timerC8() {
-if (this.textContent === "10 Min Timer"){
-let interval = setInterval(function() {
-    if (timer8value > 0) {
-    timer8value-=1;
-    let minute = Math.floor(timer8value / 60);
-    let seconds = timer8value % 60;
+  if (this.textContent === "10 Min Timer") {
+    let interval = setInterval(function () {
+      if (timer8value > 0) {
+        timer8value -= 1;
+        let minute = Math.floor(timer8value / 60);
+        let seconds = timer8value % 60;
 
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    timer8.textContent =  `${minute}:${seconds}`;
-} else{
-    clearInterval(interval);
-}
- }, 1000)
-}
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        timer8.textContent = `${minute}:${seconds}`;
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
 }
 timer8.addEventListener("click", timerC8);
 
-
-const timer9 = document.querySelector("#timer9")
+const timer9 = document.querySelector("#timer9");
 
 let timer9value = 12600;
 function timerC9() {
-if (this.textContent === "3 1/2 Hour Timer"){
-let interval = setInterval(function() {
-    if (timer9value > 0) {
-    timer9value-=1;
-    let hours = Math.floor(timer9value / 3600);
-    let minutes = Math.floor(( timer9value % 3600) /60);
-    let seconds = timer9value % 60;
+  if (this.textContent === "3 1/2 Hour Timer") {
+    let interval = setInterval(function () {
+      if (timer9value > 0) {
+        timer9value -= 1;
+        let hours = Math.floor(timer9value / 3600);
+        let minutes = Math.floor((timer9value % 3600) / 60);
+        let seconds = timer9value % 60;
 
-    minutes= minutes <10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    timer9.textContent =  `${hours}:${minutes}:${seconds}`;
-} else{
-    clearInterval(interval);
-}
- }, 1000)
-}
+        timer9.textContent = `${hours}:${minutes}:${seconds}`;
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
 }
 timer9.addEventListener("click", timerC9);
